98fc4d060ebfe1c3c12abd736a5a904f6fd1700a
3ll3d00d <mattkhan@gmail.com>
2021-05-29 11:23:23 +0100

    use a text input for gains

--- ui/src/components/Gain.js
+++ ui/src/components/Gain.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import clsx from 'clsx';
 import {makeStyles} from '@material-ui/core/styles';
 import {
@@ -43,15 +43,67 @@ const TightTextField = withStyles({
     }
 })(TextField);
 
-const GainInput = ({fieldName, helpText, minGain, maxGain, step, savedValues, values, setMV, setMute}) => {
+const GainInput = ({fieldName, helpText, minGain, maxGain, step, dp, savedValues, values, setMV, setMute}) => {
     const classes = useStyles();
+    const decimalSeparator = 1.1.toLocaleString().substring(1, 2);
+    const oneNum = '^[0-9]$';
+    console.log(decimalSeparator);
+    const sepMatcher = decimalSeparator === '.' ? /\./g : new RegExp(decimalSeparator, 'g');
+    const roundFactor = 1 / step;
     const delta = (parseFloat(values.mv) !== parseFloat(savedValues.mv) || values.mute !== savedValues.mute);
+    const setConstrainedMV = (f, dp) => {
+        const rounded = Math.round(f * (roundFactor)) / roundFactor;
+        if (rounded > maxGain) {
+            setMV(maxGain.toFixed(dp));
+        } else if (rounded < minGain) {
+            setMV(minGain.toFixed(dp));
+        } else {
+            setMV(rounded.toFixed(dp));
+        }
+    }
+    const interpretMV = (txt) => {
+        if (txt) {
+            if (txt === '-') {
+                setMV(txt);
+            } else if (maxGain === 0 && txt.match(oneNum)) {
+                setMV(`-${txt}`);
+            } else {
+                const dotCount = (txt.match(sepMatcher) || []).length;
+                if (dotCount === 0) {
+                    const f = parseFloat(txt);
+                    if (!isNaN(f)) {
+                        setConstrainedMV(f, 0);
+                    }
+                } else if (dotCount === 1) {
+                    if (txt.charAt(txt.length - 1) === decimalSeparator) {
+                        setMV(txt);
+                    } else {
+                        const f = parseFloat(txt);
+                        if (!isNaN(f)) {
+                            if (dp > 1) {
+                                const oldValue = values.mv;
+                                if (Math.abs(f) < Math.abs(oldValue) || txt.length < oldValue.toString().length) {
+                                    setMV(txt);
+                                } else {
+                                    setConstrainedMV(f, dp);
+                                }
+                            } else {
+                                setConstrainedMV(f, dp);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            setMV('');
+        }
+    };
     return (
         <TightTextField className={clsx(classes.sized, classes.withoutLabel, classes.tightPad)}
                         variant={'outlined'}
                         id={fieldName}
                         value={values.mv}
-                        onChange={e => e.target.value ? setMV(parseFloat(e.target.value)) : setMV(0)}
+                        onChange={e => interpretMV(e.target.value)}
                         aria-describedby={`${fieldName}-helper-text`}
                         InputProps={{
                             endAdornment: (
@@ -60,20 +112,18 @@ const GainInput = ({fieldName, helpText, minGain, maxGain, step, savedValues, va
                                                 onClick={e => setMute(!values.mute)}
                                                 className={classes.zeroPad}
                                                 color={delta ? 'secondary' : 'default'}>
-                                        {values.mute ? <VolumeOffIcon fontSize="small"/> : <VolumeUpIcon fontSize="small"/>}
+                                        {values.mute ? <VolumeOffIcon fontSize="small"/> :
+                                            <VolumeUpIcon fontSize="small"/>}
                                     </IconButton>
                                 </InputAdornment>
                             )
                         }}
                         inputProps={{
                             'aria-label': fieldName,
-                            min: minGain,
-                            max: maxGain,
-                            step: step,
-                            pattern: '^-?[0-9]+(\\.?[0-9]+)?$'
+                            pattern: '',
+                            inputMode: 'numeric'
                         }}
                         margin={'dense'}
-                        type='number'
                         size={'small'}
                         label={helpText}/>
     );
@@ -81,22 +131,29 @@ const GainInput = ({fieldName, helpText, minGain, maxGain, step, savedValues, va
 
 const Gain = ({selectedSlotId, deviceGains, gains, setGains, sendGains, isActive}) => {
     const classes = useStyles();
+    const [valid, setValid] = useState(true);
+    useEffect(() => {
+        setValid(isNaN(gains.master_mv) || isNaN(gains.inputOne_mv) || isNaN(gains.inputTwo_mv));
+    }, [gains]);
     if (selectedSlotId > -1) {
         return (
             <div className={classes.padTop}>
                 <FormControl component="fieldset">
                     <FormGroup row>
-                        <GainInput fieldName='master-gain' helpText='Master' minGain={-127} maxGain={0} step={0.5}
+                        <GainInput fieldName='master-gain' helpText='Master'
+                                   minGain={-127} maxGain={0} step={0.5} dp={1}
                                    savedValues={{mv: deviceGains.master_mv, mute: deviceGains.master_mute}}
                                    values={{mv: gains.master_mv, mute: gains.master_mute}}
                                    setMV={v => setGains({...gains, ...{master_mv: v}})}
                                    setMute={v => setGains({...gains, ...{master_mute: v}})}/>
-                        <GainInput fieldName='input1-gain' helpText='Input 1' minGain={-72} maxGain={12} step={0.25}
+                        <GainInput fieldName='input1-gain' helpText='Input 1'
+                                   minGain={-72} maxGain={12} step={0.25} dp={2}
                                    savedValues={{mv: deviceGains.inputOne_mv, mute: deviceGains.inputOne_mute}}
                                    values={{mv: gains.inputOne_mv, mute: gains.inputOne_mute}}
                                    setMV={v => setGains({...gains, ...{inputOne_mv: v}})}
                                    setMute={v => setGains({...gains, ...{inputOne_mute: v}})}/>
-                        <GainInput fieldName='input2-gain' helpText='Input 2' minGain={-72} maxGain={12} step={0.25}
+                        <GainInput fieldName='input2-gain' helpText='Input 2'
+                                   minGain={-72} maxGain={12} step={0.25} dp={2}
                                    savedValues={{mv: deviceGains.inputTwo_mv, mute: deviceGains.inputTwo_mute}}
                                    values={{mv: gains.inputTwo_mv, mute: gains.inputTwo_mute}}
                                    setMV={v => setGains({...gains, ...{inputTwo_mv: v}})}
@@ -106,7 +163,9 @@ const Gain = ({selectedSlotId, deviceGains, gains, setGains, sendGains, isActive
                                 color="primary"
                                 className={classes.sized}
                                 onClick={() => sendGains(selectedSlotId, gains)}
-                                startIcon={isActive() ? <CircularProgress size={24}/> : <PublishIcon fontSize="small"/>}>
+                                disabled={valid}
+                                startIcon={isActive() ? <CircularProgress size={24}/> :
+                                    <PublishIcon fontSize="small"/>}>
                             Apply
                         </Button>
                     </FormGroup>
